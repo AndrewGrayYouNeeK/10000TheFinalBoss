d3d605f8bd2f665654ed0d8d5b0470c3dadc663a
Andrew <youneekartifacts@gmail.com>
2026-06-11 22:32:11 -0500

    Remove unused sound effect components and related audio context logic; update Podfile.lock and package-lock.json to reflect changes in dependencies.

--- src/components/sfx/DiceRollSfxButtons.jsx
+++
@@ -1,6 +0,0 @@
-import SfxPreviewButtons from "@/components/sfx/SfxPreviewButtons";
-import { DICE_ROLL_SFX_PREVIEWS } from "@/lib/diceRollSfxPreview";
-
-export default function DiceRollSfxButtons({ compact = false }) {
-  return <SfxPreviewButtons items={DICE_ROLL_SFX_PREVIEWS} compact={compact} />;
-}
--- src/components/vocal/VocalSfxButtons.jsx
+++
@@ -1,6 +0,0 @@
-import SfxPreviewButtons from "@/components/sfx/SfxPreviewButtons";
-import { VOCAL_SFX_PREVIEWS } from "@/lib/vocalSfxPreview";
-
-export default function VocalSfxButtons({ compact = false }) {
-  return <SfxPreviewButtons items={VOCAL_SFX_PREVIEWS} compact={compact} columns={2} />;
-}
